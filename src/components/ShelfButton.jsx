5e57726c2d82abcfcf6ac34be4237f42b102e771
ScottWoodhams <scottwoodhams@hotmail.com>
2022-12-04 15:00:01 +0000

    Adding getting resources from server for the ShelfButton.jsx

--- src/components/ShelfButton.jsx
+++ src/components/ShelfButton.jsx
@@ -1,12 +1,65 @@
 import React from "react";
 
 
-
 export const ShelfButton = (props) => {
-  return (
-    <div className={"shelfButton"}>
-      {props.item.Name}
 
+
+
+    async function getResourceFromServer(resourceType){
+
+        let url = "";
+        if(resourceType === "icon"){
+            url = 'https://raw.githubusercontent.com/ScottWoodhams/PS-UI-Exporter/main/plugin/icons/icon_D.png'
+        } else if (resourceType === "script") {
+            url = props.item.scriptPath;
+        }
+
+        const options = {
+            method: "GET"
+        }
+
+        let response = await fetch(url, options)
+
+        if (response.status === 200) {
+
+            const imageBlob = await response.blob()
+            return URL.createObjectURL(imageBlob)
+        }
+        else {
+            console.log("HTTP-Error: " + response.status)
+            return ""
+        }
+    }
+
+    function runCode(){
+        let scriptURL = getResourceFromServer('script')
+        loadScript(scriptURL, () =>{
+
+        })
+    }
+
+    /**
+     * Used to load and execute javascript file.
+     * @param file JS file name
+     * @param callback Subscribe to get notified when script file is loaded
+     **/
+    function loadScript(file, callback) {
+        let script = document.createElement("script");
+        script.src = file;
+
+        script.onload = function () {
+            callback();
+        };
+
+        // append and execute script
+        document.documentElement.firstChild.appendChild(script);
+    }
+
+
+    return (
+    <div className={"shelfButton"} onClick={runCode()}>
+        {<image src={getResourceFromServer('icon')}> </image>}
+        <sp-label>{props.item.Name}</sp-label>
     </div>
   )
 }
