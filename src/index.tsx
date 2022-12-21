bb7a37dc6563cfb5e7d928341ca111caf828dd53
ScottWoodhams <scottwoodhams@hotmail.com>
2022-12-21 16:53:45 +0000

    vscode setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,70 +1,82 @@
-import '@babel/polyfill';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import "@babel/polyfill";
+import ReactDOM from "react-dom";
+import React from "react";
 
 import { PanelController } from "./controllers/PanelController.jsx";
 
 import { entrypoints } from "uxp";
-import {Library} from "./panels/Library";
+import { Library } from "./panels/Library";
 import {
-    isNewManifestVersionAvailable,
-    retrieveLocalManifest,
-    retrieveManifestLink, storeManifestFromServer,
-    storeManifestLink,
+	isNewManifestVersionAvailable,
+	retrieveLocalManifest,
+	retrieveManifestLink,
+	storeManifestFromServer,
+	storeManifestLink
 } from "./DataHandler";
-import {CommandController} from "./controllers/CommandController";
-import { UpdateManifestLinkDialog} from "./panels/UpdateManifestLink";
-
+import { CommandController } from "./controllers/CommandController";
+import { UpdateManifestLinkDialog } from "./panels/UpdateManifestLink";
 
 async function updateManifest() {
-    try {
-        console.log("update manifest")
-        let updateAvailable = await isNewManifestVersionAvailable();
-        console.log(updateAvailable)
-        if(updateAvailable){
-            await storeManifestFromServer();
-            console.log("reload")
+	try {
+		console.log("update manifest");
+		let updateAvailable = await isNewManifestVersionAvailable();
+		console.log(updateAvailable);
+		if (updateAvailable) {
+			await storeManifestFromServer();
+			console.log("reload");
 
-            window.location.reload(true);
-        }
-    } catch (e) {
-        console.error(e)
-    }
+			window.location.reload(true);
+		}
+	} catch (e) {
+		console.error(e);
+	}
 }
 
-const libraryController = new PanelController(() =>
-    <Library manifest={retrieveLocalManifest()}/>,{
-    id: "library",
-    menuItems:[
-        { id: "update", label: "Update", enabled: true, checked: false, oninvoke: () => updateManifest() }
-    ]
-})
+const libraryController = new PanelController(
+	() => <Library manifest={retrieveLocalManifest()} />,
+	{
+		id: "library",
+		menuItems: [
+			{
+				id: "update",
+				label: "Update",
+				enabled: true,
+				checked: false,
+				oninvoke: () => updateManifest()
+			}
+		]
+	}
+);
 
 const manifestLinkController = new CommandController(
-    ({ dialog }) => <UpdateManifestLinkDialog dialog={dialog} currentLink={retrieveManifestLink()}/>,
-    {
-        id: "updateManifestLink",
-        title: "React Starter Plugin Demo",
-        size: { width: 480, height: 480 },
-    }
+	({ dialog }) => (
+		<UpdateManifestLinkDialog
+			dialog={dialog}
+			currentLink={retrieveManifestLink()}
+		/>
+	),
+	{
+		id: "updateManifestLink",
+		title: "React Starter Plugin Demo",
+		size: { width: 480, height: 480 }
+	}
 );
 
 // const aboutController = new CommandController()
 
-
 entrypoints.setup({
-    plugin: {
-        create(plugin) {
-            console.log("created", plugin);
-        },
-        destroy() {
-            /* optional */ console.log("destroyed");
-        }
-    },
-    commands: {
-        updateManifestLink: manifestLinkController
-    },
-    panels: {
-        library: libraryController,
-    }
+	plugin: {
+		create(plugin) {
+			console.log("created", plugin);
+		},
+		destroy() {
+			/* optional */ console.log("destroyed");
+		}
+	},
+	commands: {
+		updateManifestLink: manifestLinkController
+	},
+	panels: {
+		library: libraryController
+	}
 });
